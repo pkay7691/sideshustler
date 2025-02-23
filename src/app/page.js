185e034219e4a3a76b6f73d99959c18e2ffb5a8c
Patrick Kenny <patrickmkenny91@gmail.com>
2025-02-23 16:54:29 -0600

    font updates

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,13 @@ export default function Home() {
   return (
     
     <>
-    <div>
-      Howdy
+    <div class="text-center">
+    <h1>Weclome to <span class="">Side-Hustle.io</span></h1>
+    <h2>
+    Unlock Your Hustle, Unleash Your Potential
+    </h2>
+    <p>Discover expert tips, curated resources, and real success stories to turn your passion into profit.</p>
     </div>
-    
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
+import { Squada_One } from 'next/font/google';
 import React from 'react';
+
+
+const squadaOne = Squada_One({
+  subsets: ['latin'],
+  weight: '400',
+});
 
 const Header = () => {
   return (
     <header>
-      <nav>
+      <nav id="main-nav">
         <ul>
-          <li><a href="/">Home</a></li>
+          <li className={squadaOne.className}>Side-Hustle.io</li>
+          <li><a href="/" >Home</a></li>
           <li><a href="/about">About</a></li>
-          <li><a href="/blogs">Blogs</a></li>
           <li><a href="/contact">Contact</a></li>
+          <li><a href="/blogs">Blogs</a></li>
         </ul>
       </nav>
     </header>
